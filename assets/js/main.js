2d90c7fbf95712a51f3323a79f69e572644fab08
misaelsarm <misaelsarmiento97@gmail.com>
2019-09-03 01:23:39 -0500

    slider Finally!!
    slider Finally!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,52 +92,13 @@ const navSlide = () => {
 navSlide();
 
 
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-console.log(carouselImages);
-
-//Buttons
-
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
 
 
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+$(document).ready(() => {
+    $('#slideshow .slick').slick({
+        speed: 1000,
+        dots: true,
+        arrows: true,
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-
-//Button listeners
-
-nextBtn.addEventListener('click', () => {
-    if (counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = "transform 2s ease";
-    /* carouselSlide.style.transition = "transform 1s cubic-bezier(1, .16, .39, .87)"; */
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click', () => {
-    if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 2s ease";
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImages[counter].id === 'lastClone') {
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - 2;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-    }
-
-    if (carouselImages[counter].id === 'firstClone') {
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-    }
+    });
 });
